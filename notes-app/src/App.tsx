184eb47263a01b9734a04dce9bb0f88d4ae47ef1
Praveen <spraveenitpro@gmail.com>
2024-03-25 00:04:10 +0530

    Scaffold react notes app

---
+++ notes-app/src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+	return (
+
+		<div className="AppContainer">
+			<form className="note-form" >
+				<input placeholder="Title" required />
+				<textarea placeholder="Content" rows={10} required />
+				<button type="submit">Add Note!</button>
+			</form>
+
+			<div className="notes-grid">
+				<div className="note-item">
+					<div className="notes-header">
+						<button>x</button>
+					</div>
+					<h2>Note Title</h2>
+					<p>Note content</p>
+				</div>
+			</div>
+		</div>
+
+	);
+}
+
+export default App;
