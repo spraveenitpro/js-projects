07b1fefc65f0c968d75999f9823c9aa79eee8ffb
Praveen <spraveenitpro@gmail.com>
2024-03-25 23:18:50 +0530

    Scaffold react notes app

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -1,28 +1,62 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-    return (
+	return (
 
-        <div className="AppContainer">
-            <form className="note-form" >
-                <input placeholder="Title" required />
-                <textarea placeholder="Content" rows={10} required />
-                <button type="submit">Add Note!</button>
-            </form>
+		const [notes, setNotes] = useState<Note[]>([
+		{
+			id: 1,
+			title: "test note 1",
+			content: "bla bla note1",
+		},
+		{
+			id: 2,
+			title: "test note 2 ",
+			content: "bla bla note2",
+		},
+		{
+			id: 3,
+			title: "test note 3",
+			content: "bla bla note3",
+		},
+		{
+			id: 4,
+			title: "test note 4 ",
+			content: "bla bla note4",
+		},
+		{
+			id: 5,
+			title: "test note 5",
+			content: "bla bla note5",
+		},
+		{
+			id: 6,
+			title: "test note 6",
+			content: "bla bla note6",
+		},
+	]);
 
-            <div className="notes-grid">
-                <div className="note-item">
-                    <div className="notes-header">
-                        <button>x</button>
-                    </div>
-                    <h2>Note Title</h2>
-                    <p>Note contents</p>
-                </div>
-            </div>
-        </div>
+	<div className="AppContainer">
+		<form className="note-form" >
+			<input placeholder="Title" required />
+			<textarea placeholder="Content" rows={10} required />
+			<button type="submit">Add Note!</button>
+		</form>
 
-    );
+		<div className="notes-grid">
+			<div className="note-item">
+				<div className="notes-header">
+					<button>x</button>
+				</div>
+				<h2>Note Title</h2>
+				<p>Note contents</p>
+			</div>
+		</div>
+	</div>
+
+	);
 }
 
 export default App;
