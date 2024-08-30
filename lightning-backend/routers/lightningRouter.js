6933fa5dec3ba0cb5813fc3cf975997e7c2a4c11
Praveen <spraveenitpro@gmail.com>
2024-08-29 23:32:38 -0700

    Daily update

--- lightning-backend/routers/lightningRouter.js
+++ lightning-backend/routers/lightningRouter.js
@@ -2,35 +2,37 @@
 const router = require("express").Router();
 const authenticate = require("../routers/middleware/authenticate");
 const authenticateAdmin = require("../routers/middleware/authenticateAdmin");
+const Invoice = require("../db/models/invoice.js");
+
 const {
-	getBalance,
-	createInvoice,
-	getChannelBalance,
-	payInvoice,
+    getBalance,
+    createInvoice,
+    getChannelBalance,
+    payInvoice,
 } = require("../lnd.js");
 
 
 // GET the onchain balance
 
 router.get("/balance", (req, res) => {
-	getBalance()
-		.then((balance) => {
-			res.status(200).json(balance);
-		})
-		.catch((err) => {
-			res.status(500).json(err);
-		});
+    getBalance()
+        .then((balance) => {
+            res.status(200).json(balance);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 // GET the lightning wallet balance
 router.get("/channelbalance", (req, res) => {
-	getChannelBalance()
-		.then((channelBalance) => {
-			res.status(200).json(channelBalance);
-		})
-		.catch((err) => {
-			res.status(500).json(err);
-		});
+    getChannelBalance()
+        .then((channelBalance) => {
+            res.status(200).json(channelBalance);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 
@@ -39,38 +41,59 @@ router.get("/channelbalance", (req, res) => {
 // GET all invoices from database
 
 router.get("/invoices", (req, res) => {
-	res.status(200).json({ message: "Hello World!" });
+    // Call the 'findAll' method from our Invoice model. This method retrieves all invoice records from the database.
+    Invoice.findAll()
+        .then((invoices) => {
+            // If the promise resolves (i.e., the operation was successful), we send back a response with a status of 200 (OK)
+            // and the list of invoices retrieved from the database.
+            res.status(200).json(invoices);
+        })
+        .catch((err) => {
+            // If the promise is rejected (i.e., the operation fails), we send back a response with a status of 500 (Internal Server Error)
+            // and the error that occurred. This might be due to a database issue, a network issue, etc.
+            res.status(500).json(err);
+        });
 });
 
 // POST required info to create an invoice
 router.post("/invoice", authenticate, (req, res) => {
-	const { value, memo } = req.body;
+    // The 'authenticate' middleware function is called before the main handler.
+    // This function checks if the user making the request is authenticated.
 
-	createInvoice({ value, memo })
-		.then((invoice) => {
-			res.status(200).json(invoice);
-		})
-		.catch((err) => {
-			res.status(500).json(err);
-		});
+    // Extract 'value', 'memo', and 'user_id' properties from the body of the incoming request.
+    const { value, memo, user_id } = req.body;
+    // Call the 'createInvoice' function, passing the extracted properties as an object.
+    // This function creates a new invoice record in the database.
+    createInvoice({ value, memo, user_id })
+        .then((invoice) => {
+            // If the promise resolves (i.e., the operation was successful), we send back a response with a status of 200 (OK)
+            // and the invoice object retrieved from the database.
+            res.status(200).json(invoice);
+        })
+        .catch((err) => {
+            // If the promise is rejected (i.e., the operation fails), we send back a response with a status of 500 (Internal Server Error)
+            // and the error that occurred. This could be due to a database issue, a network issue, etc.
+            res.status(500).json(err);
+        });
 });
+
 
 
 // POST an invoice to pay
 router.post("/pay", authenticateAdmin, async (req, res) => {
-	const { payment_request } = req.body;
+    const { payment_request } = req.body;
 
-	const pay = await payInvoice({ payment_request });
+    const pay = await payInvoice({ payment_request });
 
-	if (pay.payment_error) {
-		res.status(500).json(pay.payment_error);
-	}
+    if (pay.payment_error) {
+        res.status(500).json(pay.payment_error);
+    }
 
-	if (pay?.payment_route) {
-		// Save to DB
+    if (pay?.payment_route) {
+        // Save to DB
 
-		res.status(200).json(pay);
-	}
+        res.status(200).json(pay);
+    }
 });
 
 
